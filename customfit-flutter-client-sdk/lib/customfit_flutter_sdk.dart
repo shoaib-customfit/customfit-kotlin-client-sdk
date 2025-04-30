9665d4ddc2932511612fabac40cea25b0e868edf
shoaib-customfit <shoaib@customfit.ai>
2025-04-30 13:45:18 +0530

    flutter commit

---
+++ customfit-flutter-client-sdk/lib/customfit_flutter_sdk.dart
@@ -0,0 +1,21 @@
+library customfit_flutter_sdk;
+
+// Core exports
+export 'src/core/customfit.dart';
+export 'src/core/error/cf_result.dart';
+export 'src/core/model/cf_user.dart';
+
+// Config exports
+export 'src/config/core/cf_config.dart';
+
+// Event exports
+export 'src/analytics/event/event_data.dart';
+export 'src/analytics/event/event_type.dart';
+
+// Error handling exports
+export 'src/core/error/error_category.dart';
+export 'src/core/error/error_severity.dart';
+
+// Platform exports
+export 'src/core/model/device_context.dart';
+export 'src/core/model/application_info.dart';
